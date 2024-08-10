ca3c137aa40788d09215160151bebaf98357fcac
tarushi29 <128239884+tarushi29@users.noreply.github.com>
2024-08-10 20:34:32 +0530

    Add files via upload

---
+++ src/apicalls/users.js
@@ -0,0 +1,7 @@
+//this contains user related endpoints
+
+const { apiRequest } = require(".");
+
+export const RegisterUser = async (payload) => apiRequest('post', '/api/users/register', payload);
+export const LoginUser = async (payload) => apiRequest('post', '/api/users/login', payload);
+export const GetLoggedInUser = async () => apiRequest('get', '/api/users/get-logged-in-user');
